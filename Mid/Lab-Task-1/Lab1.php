ae9ef00b16c5819d7cc82a6dcca440d30549a3dc
hridoy8220 <140155955+hridoy8220@users.noreply.github.com>
2025-11-02 14:41:26 +0600

    add form

--- Mid/Lab-Task-1/Lab1.php
+++ Mid/Lab-Task-1/Lab1.php
@@ -6,8 +6,60 @@
  <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
-
- <h1>hello</h1>
     
+
+<h1>Clinic patient Registration</h1>   
+
+
+    <table>
+   <tr>
+    <td><p>Full Name:</p>  <input type="text"> </td>
+    
+   </tr>
+   
+   
+   <tr>
+    <td><p>Age:</p>  <input type="text"> </td>
+    
+   </tr>
+
+   
+   <tr>
+    <td><p>Phone Number: </p>  <input type="number"> </td>
+    
+   </tr>
+
+   
+   <tr>
+    <td><p>Email Address</p>  <input type="email"> </td>
+    
+   </tr>
+
+   
+    
+
+   <tr>
+   
+    <td>
+        <p> Insurance Provider:</p>
+        <select >
+            <option>select</option>
+        </select>
+    </td>
+   </tr>
+
+   <tr>
+    <td><p>Insurance Policy Number: </p> <input type="text"></td>
+   </tr>
+
+    <td>   <h5>Additional Information</h5>
+</td>  
+
+<tr><td><p>Use Name: </p><input type="text"></td></p></tr>
+<tr><td><p>Password: </p><input type="password"></td></tr>
+<tr><td><p>Confirm</p><input type="password"></td></tr>
+</table>
+
+
 </body>
 </html>
